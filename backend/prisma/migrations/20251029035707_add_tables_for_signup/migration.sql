234ffe9948d731b17d0b64d1ffb99fe7106edf41
Faran03 <faranahmadf@gmail.com>
2025-11-03 16:06:09 +0700

    add signup page and signin page

--- backend/prisma/migrations/20251029035707_add_tables_for_signup/migration.sql
+++ backend/prisma/migrations/20251029035707_add_tables_for_signup/migration.sql
@@ -1,27 +1,6 @@
 -- CreateEnum
-CREATE TYPE "Categories" AS ENUM ('Music', 'Education', 'Business', 'Technology', 'Sports', 'Art', 'Festival', 'Workshop', 'Conference', 'Community', 'Charity', 'Entertainment', 'Health', 'Food', 'Travel', 'Gaming', 'Fashion', 'Film', 'Literature', 'Religion');
-
--- CreateEnum
 CREATE TYPE "UserType" AS ENUM ('Customer', 'EventOrganizer');
 
--- CreateTable
-CREATE TABLE "Event" (
-    "id" TEXT NOT NULL,
-    "category" "Categories",
-    "title" TEXT NOT NULL,
-    "imgUrl" TEXT,
-    "content" TEXT,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
-    "location" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "totalTickets" INTEGER NOT NULL,
-    "availableTickets" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateTable
 CREATE TABLE "User" (
